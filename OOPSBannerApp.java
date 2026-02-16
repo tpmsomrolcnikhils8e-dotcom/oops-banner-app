b1874848c3bc7a44978ba767669fd5d447fb23f6
tpmsomrolcnikhils8e-dotcom <tpmsomrolc.nikhils8e@gmail.com>
2026-02-16 18:37:29 +0530

    UC1: Print literal text OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp
+ * 
+ * UC1 - Print literal text "OOPS" to console.
+ * 
+ * @author Swami
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
