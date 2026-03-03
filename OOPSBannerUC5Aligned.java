152fb460896444d4ddcc47ce0aefef56414aa434
tpmsomrolcnikhils8e-dotcom <tpmsomrolc.nikhils8e@gmail.com>
2026-03-03 09:58:29 +0530

    UC5:OOPS Banner App

---
+++ OOPSBannerUC5Aligned.java
@@ -0,0 +1,21 @@
+public class OOPSBannerUC5Aligned {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+
+            String.join("   ", " ***** ", " ***** ", " ******", " ******"),
+            String.join("   ", "**   **", "**   **", "**   **", "**     "),
+            String.join("   ", "**   **", "**   **", "**   **", "**     "),
+            String.join("   ", "**   **", "**   **", "****** ", " ***** "),
+            String.join("   ", "**   **", "**   **", "**      ", "      **"),
+            String.join("   ", "**   **", "**   **", "**      ", "**   **"),
+            String.join("   ", " ***** ", " ***** ", "**      ", " ***** ")
+
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
